dc0ac5b987b31ad2daf7206ebe822c65b367762c
Umesh9045 <umeshsonawane9045@gmail.com>
2025-02-17 10:59:40 +0530

    file structure and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MembersComponent } from './component/members/members.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: MembersComponent},
+    {path: "Members", component: MembersComponent}
+    
+];
